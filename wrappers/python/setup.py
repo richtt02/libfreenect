e7fe1ad95183bf2c148a0c43fad9217e5172d04f
richtt02 <97823520+richtt02@users.noreply.github.com>
2024-10-02 14:29:58 -0500

    Update setup.py
    edit

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -11,45 +11,60 @@ def get_cython_version():
         Version as a pair of ints (major, minor)
 
     Raises:
-        ImportError: Can't load cython or find version
+        ImportError: Can't load Cython or find version
     """
+    import Cython
+
     try:
-        # Modern way to get Cython version
+        # Use Cython's version attribute directly
         version = Cython.__version__
     except AttributeError:
-        raise ImportError("Unable to determine Cython version")
+        raise ImportError("Cython is not installed or its version cannot be found.")
 
+    # Regular expression to extract the major and minor version numbers
     match = re.search(r'^([0-9]+)\.([0-9]+)', version)
     try:
         return [int(g) for g in match.groups()]
     except AttributeError:
-        raise ImportError("Invalid Cython version format")
+        raise ImportError("Unable to parse Cython version.")
 
 # Only use Cython if it is available, else just use the pre-generated files
 try:
     cython_version = get_cython_version()
     # Requires Cython version 0.13 and up
     if cython_version[0] == 0 and cython_version[1] < 13:
-        raise ImportError("Cython version too old")
+        raise ImportError("Cython version is too old, need at least 0.13.")
+    
     from Cython.Distutils import build_ext
     source_ext = '.pyx'
     cmdclass = {'build_ext': build_ext}
 except ImportError:
+    # If Cython is not available, fall back to the pre-generated C files
     source_ext = '.c'
     cmdclass = {}
 
-ext_modules = [Extension("freenect", ["freenect" + source_ext],
-                         libraries=['usb-1.0', 'freenect', 'freenect_sync'],
-                         runtime_library_dirs=['/usr/local/lib',
-                                               '/usr/local/lib64',
-                                               '/usr/lib/'],
-                         extra_compile_args=['-fPIC', '-I', '../../include/',
-                                             '-I', '/usr/include/libusb-1.0/',
-                                             '-I', '/usr/local/include/libusb-1.0',
-                                             '-I', '/usr/local/include',
-                                             '-I', '../c_sync/',
-                                             '-I', np.get_include()])]
+# Define the extension modules
+ext_modules = [
+    Extension(
+        "freenect", 
+        ["freenect" + source_ext],
+        libraries=['usb-1.0', 'freenect', 'freenect_sync'],
+        runtime_library_dirs=['/usr/local/lib', '/usr/local/lib64', '/usr/lib/'],
+        extra_compile_args=[
+            '-fPIC', 
+            '-I', '../../include/',
+            '-I', '/usr/include/libusb-1.0/',
+            '-I', '/usr/local/include/libusb-1.0',
+            '-I', '/usr/local/include',
+            '-I', '../c_sync/',
+            '-I', np.get_include()
+        ]
+    )
+]
 
-setup(name='freenect',
-      cmdclass=cmdclass,
-      ext_modules=ext_modules)
+# Setup configuration
+setup(
+    name='freenect',
+    cmdclass=cmdclass,
+    ext_modules=ext_modules
+)
